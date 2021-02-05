96dea8c118dce954c8b7a3c1a1e217e612fb8b36
Niklas Arnitz <niklasarnitz@swdec.de>
2021-02-05 10:24:47 +0100

    Added Function to functions.php

--- custom/functions.php
+++ custom/functions.php
@@ -15,3 +15,43 @@ if ( ! defined( 'ABSPATH' ) ) {
  * functions.php
  * Add PHP snippets here
  */
+
+
+$allowed_thursdays_fridays_ids = [84, 83];
+$allowed_sunday_ids = [86, 85];
+
+add_filter( 'woocommerce_add_to_cart_validation', 'checkCartContents', 99, 2 );
+function checkCartContents($passed, $added_product_id) {
+	$items = [];
+	foreach( WC()->cart->get_cart() as $cart_item ){
+    	array_push($items, $cart_item['product_id']);
+	}
+
+	if(in_array($added_product_id, $allowed_thursdays_fridays_ids)) {
+		$wrongItemIsInCart = false;
+		foreach($items as $item) {
+			if(in_array($item, $allowed_sunday_ids)) {
+				$wrongItemIsInCart = true;
+			}
+		}
+
+		if($wrongItemIsInCart) {
+			wc_empty_cart();
+			return $passed;
+		}
+	} elseif(in_array($added_product_id, $allowed_sundays_ids)) {
+		$wrongItemIsInCart = false;
+		foreach($items as $item) {
+			if(in_array($item, $allowed_thursdays_fridays_ids)) {
+				$wrongItemIsInCart = true;
+			}
+		}
+
+		if($wrongItemIsInCart) {
+			wc_empty_cart();
+			return $passed;
+		}
+	} else {
+		return $passed;
+	}
+}
